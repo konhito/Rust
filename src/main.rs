99135875ddd1bd2600982cee00e2e08741fcc437
Your Name <konhito02@gmail.com>
2025-08-02 20:47:20 +0530

    rust day 2

--- src/main.rs
+++ src/main.rs
@@ -38,3 +38,36 @@
 //   //if i remove unwrap its just Option<char> but with unwrap is just char
 //   println!("{}", char1);
 // }
+// fn main(){
+//   for i  in 1..100{ //100 excluded
+//     println!("{} ", i);
+//   }
+// }
+// fn main(){
+//   let sentence = String::from("My name is Aditya");
+//   let first_word = get_first_word(sentence);
+//   println!("The first word is: {}", first_word);
+// }
+
+// fn get_first_word(s:String)->String{
+//   let mut ans = String:: new();
+//   for char in s.chars(){
+//     if char == ' ' {
+//       break;
+//     }else{
+//       ans.push(char);
+//     }
+//   } 
+//   return ans;
+// }
+ 
+fn main(){
+  let a = 10;
+  let b = 20;
+  let sum  = do_sum(a , b);
+  println!("Here is your sum {sum}");
+}
+
+fn do_sum(a:i32,b:i32) -> i32 {
+  return a + b;
+}
